57379fae5b87075337d11ce9e6e560512561db45
finny <phineas.ehrlich2026@gbwl.org>
2025-04-01 12:44:39 +0000

    glorken

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Methods that are recursive");
+      // neverEnd(); causes stack overflow error
+   }
+
+   // methods that are recursive call themselves (to repeat code)
+
+   public static void neverEnd() {
+      System.out.println(":)");
+      // contains at least one call to itself in the method body :D :) D: >:O
+      neverEnd(); // if method name is in method body it is recursive
    }
 }
