f88627360ceb4a8d50da2764e4f5217a942d3db3
finny <phineas.ehrlich2026@gbwl.org>
2025-04-02 13:30:13 +0000

    yes# Please enter the commit message for your changes. Lines starting

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
    public static void main(String []args) {
       System.out.println("Methods that are recursive");
       // neverEnd(); causes stack overflow error
-      System.out.println(factorial(25));
+      System.out.println(factorial(16));
    }
 
    // methods that are recursive call themselves (to repeat code)
