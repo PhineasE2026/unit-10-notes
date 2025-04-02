1623ad55a981f71fcff2e4823bd9d2063e653de3
finny <phineas.ehrlich2026@gbwl.org>
2025-04-02 13:27:51 +0000

    recursion

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
    public static void main(String []args) {
       System.out.println("Methods that are recursive");
       // neverEnd(); causes stack overflow error
+      System.out.println(factorial(25));
    }
 
    // methods that are recursive call themselves (to repeat code)
@@ -12,4 +13,13 @@ public class Main {
       // contains at least one call to itself in the method body :D :) D: >:O
       neverEnd(); // if method name is in method body it is recursive
    }
+
+   public static int factorial(int n) {
+      if (n == 1) {
+         return 1;
+      }
+      else {
+         return n * factorial(n-1);
+      }
+   }   
 }
